a75ee3e4526914b3e4cf5b293b1ca17b98a8b93f
Keya Sukthankar <keya.sukthankar@gmail.com>
2021-10-19 21:13:46 +0530

    utilies.go added

---
+++ cmd/api/movieHandlers.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"backend/cmd/models"
+	"errors"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request){
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.logger.Println(errors.New("invalid id parameter"))
+		app.errorJSON(w, err
+		return
+	}
+	app.logger.Println("id is", id)
+	movie := models.Movie{
+		ID: id,
+		Title: "Some Movie",
+		Description: "desc",
+		Year: 1234,
+		ReleaseDate: time.Date(2021,01,01,01,0,0,0, time.Local),
+		Runtime: 234,
+		Rating: 4,
+		CreatedAt: time.Date(2021,01,01,01,0,0,0, time.Local),
+		UpdatedAt: time.Date(2021,01,01,01,0,0,0, time.Local),
+	}
+
+	err = app.writeJSON(w,http.StatusOK, movie, "LatestMovie")
+
+}
+
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request){
+
+}
